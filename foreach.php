52aaa5690ca484318514c6f732685fc06df9c2a5
Vinay Bosamiya <vinaybosamiya294@gmail.com>
2025-03-28 22:11:44 +0530

    Add files via upload

---
+++ foreach.php
@@ -0,0 +1,19 @@
+<?php
+$ar = [1,2,3,4,5,6,7,8,9,10];
+
+// use for loop and it is hard work and it is very dificult
+// for($i = 0; $i< count($ar); $i++){
+
+//     echo $ar[$i];
+//     echo "<br>";
+// }
+
+
+// use foreach loop
+// it is simple way print array in php and it is warry simple  
+foreach($ar as $i)
+{
+    echo $i;
+   echo "<br>";
+}
+?>
