130d284590899ff960b28dba6ca7cb2438bc6cc2
Vinay Bosamiya <vinaybosamiya294@gmail.com>
2025-03-29 22:22:48 +0530

    Add files via upload

---
+++ date.php
@@ -0,0 +1,11 @@
+<?php
+$d = date("U");
+// $d = date("d S F l , h:i:s A");
+$d1 = date("W");
+echo"today date is $d";
+?>
+
+<!-- these all links are for reference and inside these these link available more functions -->
+<!-- reference link 1 = https://www.php.net/manual/en/datetime.format.php -->
+ <!-- reference link 2 = https://www.php.net/manual/en/function.date.php -->
+  <!-- reference link 3 = https://www.w3schools.com/php/php_ref_overview.asp-->
