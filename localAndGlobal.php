51c85d0a567823c120123588f67ace3451c9c4b5
Vinay Bosamiya <vinaybosamiya294@gmail.com>
2025-04-02 21:50:23 +0530

    Add files via upload

---
+++ localAndGlobal.php
@@ -0,0 +1,28 @@
+<?php
+echo "local & global variable in php.<br>";
+/*
+
+ 1 - you can use the gloabal variable inside the function but you must be use (global)  keyword inside the function because global variable is declare outside of the function so that you can use global keyword inside the function you can use it.
+ 2 -  you are not use local variable because local variable is declare inside the function so that local variable is used only inside the function
+ 
+ simple meaning of local variable :- this variable declare inside of the function
+ simple meaning of global variable :- this variable declare outside of the function
+
+ */
+
+$b = 21; // global
+echo "it is global variable = ",$b; 
+
+function get()
+{
+    $a = 18; //  local 
+    echo "<br>";
+    echo "it is local variable = ",$a;
+    echo "<br>";
+    global $b;
+    echo "it is global variable inside the function = ",$b;
+}
+
+
+get();
+?>
