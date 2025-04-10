0f0a7a250d1ef9a82044ef7c4f8cbb2d71303d12
Vinay Bosamiya <vinaybosamiya294@gmail.com>
2025-04-10 22:12:05 +0530

    Add files via upload

---
+++ 19_insert_data part 2.php
@@ -0,0 +1,71 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$pas = "";
+$database = "v";
+
+$conn = mysqli_connect($servername,$username,$pas,$database);
+$fnm = "VINAY";
+$lnm = "katri";
+$sql = "INSERT INTO `vinay`(`First Name`,`Last Name`) VALUES('$fnm','$lnm')";
+
+$result = mysqli_query($conn,$sql);
+if ($result) {
+    die("value is inserted")."<br>";
+}
+else{
+    echo "value is not inserted ".mysqli_error($conn)."<br>";
+}
+
+
+?>
+
+<!--<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>get and post method</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+
+<body>
+    <form action="getAndPost.php" method="post">
+    <form action="getAndPost.php" method="get"> 
+        <label for="fname">email</label>
+        <input type="text" name="fname"> <br>
+        <label for="lname">lname</label>
+        <input type="text" name="lname">
+       <button >submit</button>
+        <input type="reset" value="reset">
+    </form>
+
+
+
+ 
+// $servername = "localhost";
+// $username = "root";
+// $pas = "";
+// $database = "v";
+
+// $fname = 'fname';
+// $lname = 'lname';
+
+// $conn = mysqli_connect($servername,$username,$pas,$database);
+
+// $sql = "INSERT INTO `vinay`(`First Name`,`Last Name`) VALUES('$fname','$lname')";
+
+// $result = mysqli_query($conn,$sql);
+// if ($fname && $lname) {
+//     die("value is inserted")."<br>";
+// }
+// else{
+//     echo "value is not inserted ".mysqli_error($conn)."<br>";
+// }
+
+
+ 
+ </body>
+
+</html> -->
