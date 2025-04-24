0ecee1fd0e54b424d4aa0ff331e921ca4ef63c0b
Vinay Bosamiya <vinaybosamiya294@gmail.com>
2025-04-24 20:26:59 +0530

    Add files via upload

---
+++ 25(#33)_require_include.php
@@ -0,0 +1,8 @@
+<?php
+// include is the include your file and this file is available or not after your code is run now 
+include "example1.php";
+// require is function in which your file is required after all code run now 
+// require "example1.php"; 
+echo "<br>";
+echo "this";
+?>
