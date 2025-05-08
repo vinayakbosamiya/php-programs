80c926332091a983daff69f78c6a8a2b918988c4
Vinay Bosamiya <vinaybosamiya294@gmail.com>
2025-05-08 16:24:35 +0530

    Add files via upload

---
+++ 28(#36)_file.php
@@ -0,0 +1,38 @@
+<?php
+$a = fopen("28(#36)_file.txt","r");
+
+// $co = fread($a,filesize("example.txt"));
+// echo $co;
+// fgets function for 
+// // echo fgets($a);
+// echo fgets($a);
+// echo fgets($a);
+// echo fgets($a);
+// echo fgets($a);
+// while($b = fgets($a)){
+    
+//     echo $b;
+//     if ($b === ".") {   
+//         # code...
+//         break;
+//     }
+// }
+
+// fgetc for file read
+// echo fgetc($a);
+// echo fgetc($a);
+// echo fgetc($a);
+// echo fgetc($a);
+// echo fgetc($a);
+// while($b = fgetc($a)){
+    
+//     echo $b;
+//     if ($b === ".") {   
+//         # code...
+//         break;
+//     }
+// }
+// // echo fgetc($a);
+fclose($a);
+
+?>
